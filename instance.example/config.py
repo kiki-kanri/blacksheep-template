6b7706b78a5d765de0b130558a80dbd1e87dfb55
kiki-kanri <a470666@gmail.com>
2022-11-23 12:28:20 +0800

    Setup database

--- instance.example/config.py
+++ instance.example/config.py
@@ -1,3 +1,14 @@
+DATABASES = {
+    'default': {
+        'authentication_source': 'admin',
+        'db': '',
+        'host': '127.0.0.1',
+        'password': '',
+        'port': 27017,
+        'username': ''
+    }
+}
+
 INSTALLED_APPS = [
 ]
 
--- main/app.py
+++ main/app.py
@@ -1,8 +1,13 @@
 from blacksheep import Application
 from blacksheep.server.controllers import APIController, Controller
+from mongoengine import connect
 
-from instance.config import INSTALLED_APPS, SIGNING_KEY
+from instance.config import DATABASES, INSTALLED_APPS, SIGNING_KEY
 from library.utils import import_attribute
+
+
+def connect_db():
+    connect(DATABASES['default'])
 
 
 def install_apps(app: Application):
